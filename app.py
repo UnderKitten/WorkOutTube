ecb125391f35686402c2acff28d8d14c54ef01b2
Maxim Churzin <mchurzin@outlook.com>
2022-04-01 17:56:10 -0400

    IDE trouble shooting, proper page view and button connection

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class Workout(workout_db.Model):
     description = workout_db.Column(workout_db.String(300), nullable=False)
     category = workout_db.Column(workout_db.String(10), nullable=False)
     url = workout_db.Column(workout_db.Text, nullable=False)
+    duration = workout_db.Column(workout_db.Integer, nullable=False)
     date = workout_db.Column(workout_db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -43,9 +44,10 @@ def add_workout():
         title = request.form['title']
         description = request.form['description']
         category = request.form['category']
-        url = request.form['url']
+        url = convert_url_to_embed(request.form['url'])
+        duration = request.form['duration']
 
-        workout = Workout(title=title, description=description, category=category, url=url)
+        workout = Workout(title=title, description=description, category=category, url=url, duration=duration)
 
         try:
             workout_db.session.add(workout)
@@ -57,14 +59,15 @@ def add_workout():
         return render_template("add_workout.html")
 
 
-@app.route('/add_workout', methods=['POST', 'GET'])
-def edit_workout():
+@app.route('/library/<int:id>/edit', methods=['POST', 'GET'])
+def edit_workout(id):
     workout = Workout.query.get(id)
     if request.method == "POST":
         workout.title = request.form['title']
         workout.description = request.form['description']
         workout.category = request.form['category']
-        workout.url = request.form['url']
+        workout.url = convert_url_to_embed(request.form['url'])
+        workout.duration = request.form['duration']
 
         try:
             workout_db.session.commit()
@@ -72,7 +75,23 @@ def edit_workout():
         except:
             return 'Something went wrong!'
     else:
-        return render_template("edit_workout.html")
+        return render_template("edit-workout.html", workout=workout)
+
+
+@app.route('/library/<int:id>/remove')
+def remove_workout(id):
+    workout_to_remove = Workout.query.get(id)
+    try:
+        workout_db.session.delete(workout_to_remove)
+        workout_db.session.commit()
+        return redirect('/library')
+    except:
+        return "Something went wrong"
+
+
+def convert_url_to_embed(url):
+    url = url.replace("watch?v=", "embed/")
+    return url
 
 
 if __name__ == "__main__":
