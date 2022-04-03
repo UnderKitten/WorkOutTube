31884b8d9feadbc9aae521f11050f1fe1217f35c
Maxim Churzin <mchurzin@outlook.com>
2022-04-03 16:33:18 -0400

    Introduced WTForms and replaced code in add/edit templates

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, URLField, SubmitField, SelectField
+from wtforms.validators import DataRequired, url, InputRequired
+import creds
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout_list.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 workout_db = SQLAlchemy(app)
-
-
-class Workout(workout_db.Model):
-    id = workout_db.Column(workout_db.Integer, primary_key=True)
-    title = workout_db.Column(workout_db.String(50), nullable=False)
-    description = workout_db.Column(workout_db.String(300), nullable=False)
-    category = workout_db.Column(workout_db.String(10), nullable=False)
-    url = workout_db.Column(workout_db.Text, nullable=False)
-    duration = workout_db.Column(workout_db.Integer, nullable=False)
-    date = workout_db.Column(workout_db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Article {self.id}>'
+app.config['SECRET_KEY'] = creds.secret_key
 
 
 @app.route('/')
@@ -40,15 +32,11 @@ def workout_detail(id):
 
 @app.route('/add_workout', methods=['POST', 'GET'])
 def add_workout():
+    new_workout = WorkoutForm()
     if request.method == "POST":
-        title = request.form['title']
-        description = request.form['description']
-        category = request.form['category']
-        url = convert_url_to_embed(request.form['url'])
-        duration = request.form['duration']
-
-        workout = Workout(title=title, description=description, category=category, url=url, duration=duration)
-
+        workout = Workout(title=new_workout.title.data, description=new_workout.description.data
+                          , category=new_workout.category.data, url=new_workout.url.data,
+                          duration=new_workout.duration.data)
         try:
             workout_db.session.add(workout)
             workout_db.session.commit()
@@ -56,26 +44,31 @@ def add_workout():
         except:
             return 'Something went wrong!'
     else:
-        return render_template("add_workout.html")
+        return render_template("add_workout.html", new_workout=new_workout)
 
 
 @app.route('/library/<int:id>/edit', methods=['POST', 'GET'])
 def edit_workout(id):
     workout = Workout.query.get(id)
+    edited_workout = WorkoutForm(id=workout.id, title=workout.title, description=workout.description,
+                                 category=workout.category,
+                                 url=workout.url, duration=workout.duration, date=workout.date)
     if request.method == "POST":
-        workout.title = request.form['title']
-        workout.description = request.form['description']
-        workout.category = request.form['category']
-        workout.url = convert_url_to_embed(request.form['url'])
-        workout.duration = request.form['duration']
-
-        try:
-            workout_db.session.commit()
-            return redirect('/library')
-        except:
-            return 'Something went wrong!'
+        if edited_workout.update.data:
+            workout.title = edited_workout.title.data
+            workout.description = edited_workout.description.data
+            workout.category = edited_workout.category.data
+            workout.url = convert_url_to_embed(edited_workout.url.data)
+            workout.duration = edited_workout.duration.data
+            try:
+                workout_db.session.commit()
+                return redirect('/library')
+            except:
+                return 'Something went wrong!'
+        elif edited_workout.remove.data:
+            return redirect(f'/library/{id}/remove')
     else:
-        return render_template("edit-workout.html", workout=workout)
+        return render_template("edit-workout.html", edited_workout=edited_workout)
 
 
 @app.route('/library/<int:id>/remove')
@@ -92,6 +85,32 @@ def remove_workout(id):
 def convert_url_to_embed(url):
     url = url.replace("watch?v=", "embed/")
     return url
+
+
+class Workout(workout_db.Model):
+    id = workout_db.Column(workout_db.Integer, primary_key=True)
+    title = workout_db.Column(workout_db.String(50), nullable=False)
+    description = workout_db.Column(workout_db.String(300), nullable=False)
+    category = workout_db.Column(workout_db.String(10), nullable=False)
+    url = workout_db.Column(workout_db.Text, nullable=False)
+    duration = workout_db.Column(workout_db.Integer, nullable=False)
+    date = workout_db.Column(workout_db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Article {self.id}>'
+
+
+class WorkoutForm(FlaskForm):
+    title = StringField("title", validators=[InputRequired()])
+    description = TextAreaField("description", validators=[InputRequired()])
+    category = SelectField("Workout category:",
+                           choices=[('full', 'Full Body'), ('upper', 'Upper Body'), ('lower', 'Lower Body'),
+                                    ('core', 'Core')])
+    url = URLField("url", validators=[DataRequired(), url()])
+    duration = IntegerField("duration", validators=[InputRequired()])
+    submit = SubmitField("Submit")
+    update = SubmitField("Edit")
+    remove = SubmitField("Remove")
 
 
 if __name__ == "__main__":
